af6e002388b1b872e271da18b04430dd232f7438
bechampion <garciaj.uk@gmail.com>
2021-11-30 14:43:02 +0000

    adding a producer example

---
+++ producer.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/streadway/amqp"
+)
+
+var (
+	uri          = flag.String("uri", "amqp://guest:guest@localhost:5672/", "AMQP URI")
+	exchangeName = flag.String("exchange", "test-exchange", "Durable AMQP exchange name")
+	exchangeType = flag.String("exchange-type", "direct", "Exchange type - direct|fanout|topic|x-custom")
+	routingKey   = flag.String("key", "test-key", "AMQP routing key")
+	body         = flag.String("body", "foobar", "Body of message")
+	reliable     = flag.Bool("reliable", false, "Wait for the publisher confirmation before exiting")
+)
+
+func init() {
+	flag.Parse()
+}
+
+func main() {
+	if err := publish(*uri, *exchangeName, *exchangeType, *routingKey, *body, *reliable); err != nil {
+		log.Fatalf("%s", err)
+	}
+	log.Printf("published %dB OK", len(*body))
+}
+
+func publish(amqpURI, exchange, exchangeType, routingKey, body string, reliable bool) error {
+
+	log.Printf("dialing %q", amqpURI)
+	connection, err := amqp.Dial(amqpURI)
+	if err != nil {
+		return fmt.Errorf("Dial: %s", err)
+	}
+	defer connection.Close()
+
+	log.Printf("got Connection, getting Channel")
+	channel, err := connection.Channel()
+	if err != nil {
+		return fmt.Errorf("Channel: %s", err)
+	}
+
+	log.Printf("got Channel, declaring %q Exchange (%q)", exchangeType, exchange)
+	if err := channel.ExchangeDeclare(
+		exchange,     // name
+		exchangeType, // type
+		true,         // durable
+		false,        // auto-deleted
+		false,        // internal
+		false,        // noWait
+		nil,          // arguments
+	); err != nil {
+		return fmt.Errorf("Exchange Declare: %s", err)
+	}
+
+	channel.QueueDeclare("TestQueue", true, false, false, false, nil)
+
+	channel.QueueBind("TestQueue", "test-key", "test-exchange", false, nil)
+
+	if reliable {
+		log.Printf("enabling publishing confirms.")
+		if err := channel.Confirm(false); err != nil {
+			return fmt.Errorf("Channel could not be put into confirm mode: %s", err)
+		}
+
+		confirms := channel.NotifyPublish(make(chan amqp.Confirmation, 1))
+
+		defer confirmOne(confirms)
+	}
+
+	log.Printf("declared Exchange, publishing %dB body (%q)", len(body), body)
+	for true {
+		time.Sleep(1 * time.Second)
+		t := time.Now()
+		st := fmt.Sprintf("%d/%d/%d:%d:%d:%d", t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second())
+		body = fmt.Sprintf("%s -> foobar", st)
+		err = channel.Publish(
+			exchange,   // publish to an exchange
+			routingKey, // routing to 0 or more queues
+			false,      // mandatory
+			false,      // immediate
+			amqp.Publishing{
+				Headers:         amqp.Table{},
+				ContentType:     "text/plain",
+				ContentEncoding: "",
+				Body:            []byte(body),
+				DeliveryMode:    amqp.Transient, // 1=non-persistent, 2=persistent
+				Priority:        0,              // 0-9
+				// a bunch of application/implementation-specific fields
+			},
+		)
+		if err != nil {
+			panic(fmt.Errorf("Exchange Publish: %s", err))
+		}
+	}
+
+	return nil
+}
+
+func confirmOne(confirms <-chan amqp.Confirmation) {
+	log.Printf("waiting for confirmation of one publishing")
+
+	if confirmed := <-confirms; confirmed.Ack {
+		log.Printf("confirmed delivery with delivery tag: %d", confirmed.DeliveryTag)
+	} else {
+		log.Printf("failed delivery of delivery tag: %d", confirmed.DeliveryTag)
+	}
+}
